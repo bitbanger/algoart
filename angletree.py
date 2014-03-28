d477051bdf7a77d4b7fac0a7f6b54587e75c4b45
bitbanger <lxl5734@rit.edu>
2014-03-28 00:38:02 -0400

    Added binary circle tree art

---
+++ angletree.py
@@ -0,0 +1,84 @@
+from math import *
+from operator import concat
+from random import randint, random
+
+import pysvg.structure
+import pysvg.builders
+
+img_side = 1024
+
+img_xmid = img_side/2
+img_ymid = img_side/2
+
+starting_rad = 128
+
+class Circle:
+	__slots__ = ('x', 'y', 'rad', 'depth')
+	
+	def __init__(self, x, y, rad, depth):
+		self.x = x
+		self.y = y
+		self.rad = rad
+		self.depth = depth
+
+circles = []
+
+def rgb_to_hex(rgb):
+	return "#" + reduce(concat, map(lambda x: "%02x" % x, rgb))
+
+def make_child(circle):
+	ang = random() * (2 * pi)
+	px = (1.5 * circle.rad) * cos(ang) + circle.x
+	py = (1.5 * circle.rad) * sin(ang) + circle.y
+	
+	return Circle(px, py, circle.rad/2, circle.depth + 1)
+
+def make_tree(root, branch, depth):
+	if depth == 0:
+		return
+	
+	children = []
+	
+	for _ in range(branch):
+		child = make_child(root)
+		children.append(child)
+		circles.append(child)
+	
+	for child in children:
+		make_tree(child, branch, depth - 1)
+
+# Add the root
+root = Circle(img_xmid, img_ymid, starting_rad, 0)
+circles.append(root)
+
+# Make the tree
+depth = 6
+branching_factor = 7
+make_tree(root, branching_factor, depth)
+
+# Make the SVG
+svg = pysvg.structure.svg()
+sb = pysvg.builders.ShapeBuilder()
+
+for circ in circles:
+	darkness = (float(circ.depth) / depth) * 255
+	color = rgb_to_hex((darkness, darkness, darkness))
+	svg.addElement(sb.createCircle(circ.x, circ.y, circ.rad, strokewidth = 0, fill = color))
+
+# Hack fill opacity in because PySVG doesn't have it :(
+xml = svg.getXML().replace("; \"", "; fill-opacity:0.75; \"")
+
+with open("angletree.svg", "w") as f:
+	f.write(xml)
+
+f.close()
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
